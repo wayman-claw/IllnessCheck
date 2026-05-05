1983df66feb061a4d1583fa396aa19fa7fc57672
Timo Dachs-Wegmann <tdachs-wegma@Mac.fritz.box>
2026-05-05 10:00:08 +0200

    Added Unit Testing Bundle

---
+++ DayTraceTests/DayTraceTests.swift
@@ -0,0 +1,16 @@
+//
+//  DayTraceTests.swift
+//  DayTraceTests
+//
+//  Created by Timo Dachs-Wegmann on 05.05.26.
+//
+
+import Testing
+
+struct DayTraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
